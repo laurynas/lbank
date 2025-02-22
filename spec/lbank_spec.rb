a4b16d1fc4a3b256dcf71760e6a49cd81a030c0e
Laurynas Butkus <laurynas.butkus@gmail.com>
2025-02-22 10:00:34 +0200

    Update specs

--- spec/lbank_spec.rb
+++ spec/lbank_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
 describe Lbank do
-  describe '#currency_rates' do
-    subject { described_class.currency_rates(time) }
+  let(:date) { Date.new(2012, 8, 4) }
 
-    let(:time) {}
+  describe '#currency_rates' do
+    subject { described_class.currency_rates(date) }
 
     its(['USD']) { is_expected.to be > 0 }
 
     context 'when invalid time' do
-      let(:time) { Time.now + 1.week }
+      let(:date) { Time.now + 1.week }
 
       it 'raises error' do
         expect { subject }.to raise_error(Lbank::ResponseError, /No data/)
@@ -19,12 +19,7 @@ describe Lbank do
 
   describe '#convert_currency' do
     it 'converts currency' do
-      expect(subject.convert_currency(10, 'USD', 'EUR')).to be > 0
-      expect(subject.convert_currency(10, 'EUR', 'LTL')).to eq 34.528
-
-      date = Date.new(2012, 8, 4)
-
-      expect(subject.convert_currency(10, 'EUR', 'LTL')).to eq 34.528
+      expect(subject.convert_currency(10, 'EUR', 'LTL', date).round(2)).to eq 34.53
       expect(subject.convert_currency(1, 'USD', 'EUR', date).round(2)).to eq 0.82
       expect(subject.convert_currency(100, 'RUB', 'USD', date).round(2)).to eq 3.08
     end
